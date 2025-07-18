c8b7697621b8d334c00cfadc669627056f2cbf55
amegiasf <107200326+amegiasf@users.noreply.github.com>
2025-07-18 14:29:47 +0200

    Delete tools/s.sh

--- tools/s.sh
+++
@@ -1,8 +0,0 @@
-./run.py 9000 30 0.5 cuda:0 &
-./run.py 9000 30 0.8 cuda:1 &
-./run.py 9000 30 1.0 cuda:2 &
-./run.py 9000 30 10.0 cuda:3 &
-./run.py 9000 30 100.0 cuda:4 &
-./run.py 9000 30 1000.0 cuda:5 &
-./run.py 9000 30 10000.0 cuda:6 &
-
